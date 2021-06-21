89a4b95b10aa452c2449eda2e757dcf00c49ef4c
Christine.Hanna <christine.hany.sorial@gmail.com>
2021-06-21 06:44:48 +0200

    final-update

--- src/utilis/helpers.js
+++ src/utilis/helpers.js
@@ -24,30 +24,4 @@ export function formatQuestion ({ optionOneText, optionTwoText, author }) {
     }
   }
 
-
-  export const colors = {
-    green: {
-      name: 'green',
-      hex: '#21ba45'
-    },
-    blue: {
-      name: 'blue',
-      hex: '#2185d0'
-    },
-    grey: {
-      name: null,
-      hex: '#d4d4d5'
-    }
-  };
-  
-  export const styles = {
-    primary: {
-      color: 'green',
-      bgColor: 'honeydew'
-    },
-    secondary: {
-      color: 'grey',
-      bgColor: '#f4f4f4'
-    }
-  };
   
